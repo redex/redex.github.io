d0db2077f0dab21a81d177c9298a60732e0404c5
glennsl <glenn@slotte.net>
2020-10-12 09:13:07 +0200

    Updates

---
+++ path---keyword-xml-9334adb0b8fd5f7650d0.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xd4213d5ac91b],{1170:function(e,s){e.exports={data:{packages:{edges:[{node:{type:"published",id:"bs-pixl-xml",name:"bs-pixl-xml",version:"0.1.0",category:"binding",flags:[],platforms:["node","browser"],description:"Bucklescript bindings to pixl-xml",keywords:["xml"],license:"MIT",updated:"2020-08-27T13:50:59.370Z",stars:null,score:.1758791482455496,quality:.5842221546475592,popularity:.0017500052893766793,maintenance:0,slug:"/package/bs-pixl-xml"}}]}},pathContext:{keyword:"xml"}}}});
+//# sourceMappingURL=path---keyword-xml-9334adb0b8fd5f7650d0.js.map
